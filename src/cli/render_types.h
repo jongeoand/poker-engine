6ec81cf5248cf4c1b555b1bbe6e985145593db50
jongeoand <jonathangeorgeanderson@gmail.com>
2026-04-14 21:32:39 -0400

    future planned render types / thinking about

--- src/cli/render_types.h
+++ src/cli/render_types.h
@@ -3,13 +3,22 @@
 
 typedef enum {
 	RENDER_STATE,   /* dominant ComboState per cell */
+	RENDER_SUIT,    /* dominant SuitClass per cell */
 	RENDER_PURITY,  /* purity/fraction of dominant state */
 	RENDER_DRAW,    /* behind-live (draw) fraction */
-	RENDER_SUIT,    /* dominant SuitClass per cell */
 	RENDER_FLUSH,   /* flush-draw/made fraction */
+    RENDER_EQUITY,
+    RENDER_JOINT,
+    RENDER_ENTROPY,
+    RENDER_VOLATILITY,
 } RenderMode;
 
 typedef enum { SYMSET_ASCII, SYMSET_UNICODE } SymSet;
-typedef enum { CELL_1, CELL_2, CELL_4 }      CellWidth;
+
+typedef enum { 
+    CELL_1 = 1,
+    CELL_2 = 2, 
+    CELL_4 = 4,
+} CellWidth;
 
 #endif
